68fd525ce3109f1db215ada22f14b48a8d98fb65
Zarkopafilis <zarkopafilis@gmail.com>
2017-11-07 23:34:07 +0200

    finish fib for exc3

--- at91_asm/exc3_fib.s
+++ at91_asm/exc3_fib.s
@@ -6,14 +6,25 @@
 main:
 STMDB R13!, {R0-R12, R14}
 
-MOV R0, #0
-MOV R1, =Stor
+MOV R0, #2
+LDR R1, =Stor
 
 LOOP:
 
-STRB R0, [=Stor, R1]
+MOV R4, R0
 
-CMPB R0, #5
+@An-1
+LDRB R2, [R1, R4]
+
+ADD R4, R4, #1
+
+@An-2
+LDRB R3, [R1, R4]
+
+ADD R4, R2 , R3
+STRB R4, [R1, R4]
+
+CMP R0, #5
 ADD R0, R0, #1
 BNE LOOP
 
@@ -21,4 +32,5 @@ LDMIA R13!, {R0-R12, PC}
 
 .data
 Stor:
-.byte 0x0, 0x0, 0x0, 0x0, 0x0
+@First 2 fibonacci
+.byte 0x1, 0x2, 0x0, 0x0, 0x0
