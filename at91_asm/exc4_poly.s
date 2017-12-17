fe396c09e5521c4a6249675ae1b33227f2696d07
Zarkopafilis <zarkopafilis@gmail.com>
2017-12-17 18:06:53 +0200

    Untested exc5

--- at91_asm/exc4_poly.s
+++ at91_asm/exc4_poly.s
@@ -8,7 +8,6 @@ STMDB R13!, {R0-R12, R14}
 
 LDR R1, =Values
 LDR R2, =Const
-MOV R3, #0 @Result
 
 MOV R8, #0 @Counter
 
@@ -19,8 +18,8 @@ LDR R0, [R1, R8]
 BL Subrtn
 AfterLoop:
 
-CMP R8, #3
-ADD R8, R8, #1
+CMP R8, #12
+ADD R8, R8, #4
 
 BNE LOOP
 
@@ -33,10 +32,10 @@ MOV R4, #6 @i
 LDRB R5, [R2, R4] @Bi (b6 initial)
 
 Calc:
-CMP R4, #0
+CMP R4, #1
 
 SUB R4, R4, #1
-LDRB R6, [R2, R6] @Ai
+LDRB R6, [R2, R4] @Ai
 
 MUL R5, R0, R5
 ADD R5, R5, R6
---
+++ at91_asm/exc5_sort.s
@@ -0,0 +1,73 @@
+@BOARD AT91
+.text
+.global main
+
+main: 
+STMDB R13!, {R0-R12, R14}
+
+START:
+MOV R0, =Array
+MOV R1, #20
+
+BL Sort 
+BL Check
+
+END:
+LDMIA R13!, {R0-R12, PC}
+
+Sort:
+STMDB R13!, {R2-R6}
+
+MOV R2, #0 @Counter
+
+Loop:
+LDRB R3, [R0, R2] @Current Element
+MOV R4, R2 @Current Element Index
+
+ADD R2, R2, #1
+
+LDRB R5, [R0, R2] @Next Element
+MOV R6, R2 @Next Element Index
+
+CMPB R5, R3 @Find Greater
+
+BHI LoopEnd
+@These execute only if R3 > R5 (swap)
+STRB R3, [R0, R6]
+STRB R5, [R0, R4] 
+LoopEnd:
+
+CMP R2, R1
+BNE Loop
+
+LDMIA R13!, {R2-R6}
+MOV PC, LR
+
+Check:
+STMDB R13!, {}
+
+MOV R2, =Correct
+MOV R3, #0
+MOV R6, #0x0000
+
+CL:
+LDRB R4, [R0, R3]
+ADD R3, R3, #1
+LDRB R5, [R0, R3]
+
+CMPB R4, R5
+@If R4 > R5
+STRHI R6, [R2]
+
+CMPB R3, #20
+BNE CL
+
+LDMIA R13!, {}
+MOV PC, LR
+
+.data
+Array:
+.byte 0x02, 0x01, 0x05, 0x04, 0x03, 0x06, 0x09 , 0x08, 0x07, 0x0A, 0x0C, 0x0B, 0x0F, 0x0E, 0x0D, 0xFF, 0x20, 0x2F, 0x1F
+
+Correct:
+.word 0xFFFF
