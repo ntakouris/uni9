5ef311e246b683ea6473d7649c38b94480c61e63
Theodoros Zarkopafilis Ntakouris <zarkopafilis@gmail.com>
2018-11-10 17:40:51 +0200

    Merge parameter parsing

--- OperatingSystems/tool332432.sh
+++ OperatingSystems/tool332432.sh
@@ -7,30 +7,61 @@ if [ $# -eq 0 ]
     exit
 fi
 
-#parameter parsing
-while getopts ":f:i:s:" opt
-   do
-     case $opt in
-        f ) file=$OPTARG;;
-        i ) id=$OPTARG;;
-        s ) socialmedia='true';;
-     esac
-done
-socialmedia='asdf'
-echo $socialmedia
-
 # id|lastName|firstName|gender|birthday|joinDate|IP|browserUsed|socialmedia
 
 columnsep='\t'
+dateformat='%d/%m/%y'
 
+#parameter parsing
+while [ -n "$1" ]; do # iterates every option until no option left
+    case "$1" in
+ 
+    -f) file="$2" # gets file name from next parameter
+        shift;;
+ 
+    -id) id="$2"
+        shift;;
+ 
+    --socialmedia) socialmedia='true';; 
+ 
+    --firstnames) firstnames='true';;
+    
+    --lastnames) lastnames'true';;
+    
+    --born-since) bornsince="$2"
+            shift;;
+    
+    --born-until) bornuntil="$2"
+            shift;;
+            
+    --edit) editid="$2"
+            editcolumn="$3"
+            editvalue="$4"
+            shift 3;;
+ 
+    *) echo "Option $1 not recognized";;
+    esac
+    shift # next parameter for parsing
+done
 
-# only lines that match
-if [[ ! ${bornsince+x} ]] && [[ ! ${bornuntil+x} ]]; then
-    # sed-out date, keep
+#ignore lines that start with #
+#compare birthdates and if check pass, print line (5th column is birthday date)
+if [[ ${bornsince+x} ]] && [[ ${bornuntil+x} ]]; then
+    awk -F "$columnsep" -v dateA="$bornsince" -v dateB="$bornuntil" '{if (FNR>1 && dateA<=$5 && dateB>=$5) {print}}' $file
     exit
 fi
 
-if [[ ! ${editid+x} ]] && [[ ! ${editcolumn+x} ]] && [[ ! ${editvalue+x} ]]; then
+if [[ ${bornsince+x} ]]; then
+    awk -F "$columnsep" -v dateA="$bornsince" '{if (FNR>1 && dateA<=$5) {print}}' $file
+    exit
+fi
+
+if [[ ${bornuntil+x} ]]; then
+    awk -F "$columnsep" -v dateB="$bornuntil" '{if (FNR>1 && dateB>=$5) {print}}' $file
+    exit
+fi
+
+if [[ ${editid+x} ]] && [[ ${editcolumn+x} ]] && [[ ${editvalue+x} ]]; then
 
     exit
 fi
@@ -38,7 +69,7 @@ fi
 #ignore lines that start with #
 #keep only column of last name
 #sort alphabetically, print only discrete rows
-if [[ ! ${lastnames+x} ]]; then
+if [[ ${lastnames+x} ]]; then
     grep "^[^#]" $file |  awk -F "$columnsep" '{print $2}' | sort | uniq
     exit
 fi
@@ -46,7 +77,7 @@ fi
 #ignore lines that start with #
 #keep only column of first name
 #sort alphabetically, print only discrete rows
-if [[ ! ${firstnames+x} ]]; then
+if [[ ${firstnames+x} ]]; then
     grep "^[^#]" $file |  awk -F "$columnsep" '{print $3}' | sort | uniq
     exit
 fi
@@ -63,7 +94,7 @@ fi
 
 # echo line that starts with given id | split with column separator and print correct columns with spacing
 if [[ ${id+x} ]]; then
-    awk "/^$id/" $file | awk -F "$columnsep" '{print $3 " " $2 " " $6}'
+    awk "/^$id/" $file | awk -F "$columnsep" '{print $3 " " $2 " " $5}'
     exit
 fi
 
