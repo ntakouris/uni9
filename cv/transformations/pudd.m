fe16c563ac9db0de0f34c8e656fa12fc853cfbb5
Theodoros Zarkopafilis Ntakouris <zarkopafilis@gmail.com>
2019-11-26 11:34:29 +0200

    cv: complete pudding jiggle

--- cv/transformations/pudd.m
+++ cv/transformations/pudd.m
@@ -5,10 +5,17 @@ writerObj = VideoWriter('pudding_shear.avi');
 writerObj.FrameRate = 24;
 open(writerObj);
 
+speed = 4;
+ampl = 0.2;
+
 for f = 1:frame_count
-    shear = sin((f / frame_count) * (2*pi));
-    transform = maketform('affine2d',[1 0 0; shear 1 0; 0 0 1]);
-    frame = im2frame(imwarp(pudding, transform));
+    fprintf('Proccessing frame %d of %d\n', f, frame_count)
+    shear = ampl * sin((f / frame_count) * (2 * speed *pi));
+    
+    transform = affine2d([1 0 0; 2 * shear 1 0; 0 0 1]);
+    i = imwarp(pudding, transform, 'OutputView', imref2d( size(pudding) ));
+    size(i)
+    frame = im2frame(i);
     
     writeVideo(writerObj, frame);
 end
