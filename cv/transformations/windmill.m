9d38b102368dfb6ac00a3946efe26cff15103976
Theodoros Zarkopafilis Ntakouris <zarkopafilis@gmail.com>
2019-11-26 12:35:42 +0200

    cv: complete windmill

--- cv/transformations/windmill.m
+++ cv/transformations/windmill.m
@@ -1,29 +1,47 @@
-mill = imread('windmill.png');
-bg = imread('windmill_back.jpeg');
-mask = imread('windmill_mask.png');
+mill = im2double(imread('windmill.png'));
+bg = im2double(imread('windmill_back.jpeg'));
+mask = 1 - im2double(imread('windmill_mask.png'));
 
-frame_count = 100;
+frame_count = 10;
 
-writerObj = VideoWriter('millspin.avi');
+writerObj = VideoWriter('transf_windmill.avi');
 writerObj.FrameRate = 24;
 open(writerObj);
 
-root = [250 400];
 mill_size = size(mill);
 mill_size = mill_size(1);
 
 method = 'nearest';
 for f = 1:frame_count
-    angle = (frame / frame_count) * 360;
-    rot_mill = imrotate(mill, angle, method);
-    rot_mask = imrotate(mask, angle, method);
+    fprintf('Proccessing frame %d of %d\n', f, frame_count)
     
-    % paste image ontop of other based on mask
-    % f = ..
+    theta = (f / frame_count) * 20;
+        
+    rot_mill = imrotate(mill, theta, 'nearest', 'crop');
+    rot_mask = 1 - imrotate(mask, theta, 'nearest', 'crop');
+        
+    big_mill = zeros(size(bg));
+    big_mask = zeros(size(bg));
+    s = size(rot_mill, 1);
     
-    writeVideo(writerObj, im2frame(f));
+    root_x = 450 - int32(s/2);
+    root_y = 650 - int32(s/2);
+    
+    for x = 1:s
+        for y = 1:s
+            if rot_mask(x,y) == 0
+                big_mill(root_x + x, root_y + y, :) = rot_mill(x,y,:);
+                big_mask(root_x + x, root_y + y, :) = 1;
+            end
+        end
+    end
+    
+    i = (1-big_mask) .* bg + big_mask .* big_mill;
+    i = rescale(i); 
+    
+    writeVideo(writerObj, im2frame(i));
 end
 
 close(writerObj);
-implay('millspin.avi');
+implay('transf_windmill.avi');
 % save to transf_windmill.mat
