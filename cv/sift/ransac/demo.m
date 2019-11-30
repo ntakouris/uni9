0c9ad5e664901e44c9f20cc5012a5c3a70458992
Theodoros Zarkopafilis Ntakouris <zarkopafilis@gmail.com>
2019-11-30 10:21:00 +0200

    sift: very simple feature match

---
+++ cv/sift/ransac/demo.m
@@ -0,0 +1,39 @@
+stored_feat_l = load('sift/ransac/left.mat');
+stored_feat_r = load('sift/ransac/right.mat');
+
+feat_l = stored_feat_l.feature; % l got the most features
+feat_r = stored_feat_r.feature;
+
+feat_count = min(size(feat_l, 2), size(feat_r, 2));
+
+feat_matched = zeros(feat_count); % feat_matched(right_idx) = left_idx
+
+for i = 1:size(feat_l,2)
+    if any(feat_matched(:) == 0)
+        break;
+    end
+    
+    feat = feat_l(:, i);
+    
+    min_dist = -1;
+    min_idx = -1;
+    
+    % find a non-matched one with minimum eucl distance
+    for j = 1:feat_count % from the right ones
+        if feat_matched(j) == 0
+            feat_other = feat_r(:, j);
+            dist = norm(feat-feat_other, 2);
+            
+            if dist < min_dist
+                min_dist = dist;
+                min_idx = j;
+            end
+        end
+    end
+    
+    if min_dist >= 0 && min_idx >= 1
+        feat_matched(min_idx) = i;
+    end
+end
+
+% todo(?) visualize
