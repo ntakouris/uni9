670609dd277ec2b4fa05796a681126fea2c3fdd8
Theodoros Ntakouris <zarkopafilis@gmail.com>
2019-12-10 18:50:19 +0200

    sci: add matrix generators to make some metrics

--- scientific-calc/b2t.m
+++ scientific-calc/b2t.m
@@ -1,4 +1,11 @@
-function T = b2t(A)
+function T = b2t(A, m, n)
 % Author: ?. ?????????, ?? 1054332, Date : 10/12/2019
+    T = tenzeros([m m n n]);
 
+    for i = 1:m % n x n blocks
+        for j = 1:m
+            %fprintf("A(%d:%d, %d:%d) -> T(:,:, %d, %d)\n", ((i-1) * m) + 1, i * m,(j-1) * m + 1, j * m , i, j);
+            T(:, :, i, j) = A(((i-1) * m) + 1:(i * m), ((j-1) * m) + 1:(j * m));
+        end
+    end
 end
---
+++ scientific-calc/matrixgen.m
@@ -0,0 +1,43 @@
+% Author: T. Ntakouris - AM 1054332
+
+% Production of the matrices required at part 3.1
+N = 1000;
+theta = 1/2;
+
+A = zeros(N);
+
+for i = 1:N
+    for j = 1:N
+       A(i, j) = (i - j)^2;
+    end
+    A(i,i) = 1 + i^theta;
+end
+
+[dflag, discrC, discrR]= dd_check(A);
+fprintf("cond(A) = %d, condest(A) = %d, dflag = %d, discrC = %d, discrR = %d\n", cond(A), condest(A), dflag, discrC, discrR);
+
+% NM = [[100 10]; [10 100]; [100 100]];
+
+P_100_10 = gen_P(100, 10);
+P_10_100 = gen_P(10, 100);
+P_100_100 = gen_P(100, 100);
+
+[dflag, discrC, discrR]= dd_check(P_100_10);
+fprintf("cond(P_100_10) = %d, condest(P_100_10) = %d, dflag = %d, discrC = %d, discrR = %d\n", cond(P_100_10), condest(P_100_10), dflag, discrC, discrR);
+
+[dflag, discrC, discrR]= dd_check(P_10_100);
+fprintf("cond(P_10_100) = %d, condest(P_10_100) = %d, dflag = %d, discrC = %d, discrR = %d\n", cond(P_10_100), condest(P_10_100), dflag, discrC, discrR);
+
+% takes some half-minute
+%[dflag, discrC, discrR]= dd_check(P_100_100);
+%fprintf("cond(P_100_100) = %d, condest(P_100_100) = %d, dflag = %d, discrC = %d, discrR = %d\n", cond(P_100_100), condest(P_100_100), dflag, discrC, discrR);
+
+function P = gen_P(n, m)
+    I_n = eye(n);
+    I_m = eye(m);
+    
+    T_m = toeplitz([30 -16 1 zeros(1,m-3)]);
+    S_n = toeplitz([24 -12 zeros(1,n-2)]);
+    
+    P = sparse(kron(I_n, T_m) + kron(S_n, I_m));
+end
