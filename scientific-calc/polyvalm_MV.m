9bfdbe29bdccc7ae793793dacbf9c8c9cc5c196e
Theodoros Ntakouris <zarkopafilis@gmail.com>
2019-12-13 21:35:06 +0200

    fix polyvalm

--- scientific-calc/polyvalm_MV.m
+++ scientific-calc/polyvalm_MV.m
@@ -1,10 +1,8 @@
 function P = polyvalm_MV(p, A, b)
 % Author: T. NTAKOURIS, AM 1054332, Date : 12/12/2019
-    P = p(1) * A * b; % BLAS-2
+    P = p(1) * A * b + p(2) * b; % BLAS-2
     
-    for i = 2:(size(p, 2)-1)
-        P = A(P + p(i) * b);  % BLAS-2
+    for i = 2:size(p, 2)
+        P = A*p + p(i)*b;  % BLAS-2
     end   
-    
-    P = P + p(end) * b; % still BLAS-2
 end
