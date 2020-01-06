21de47ea80f79896a470e0b9b8648eeabb193b27
Theodoros Zarkopafilis Ntakouris <zarkopafilis@gmail.com>
2020-01-06 14:08:28 +0200

    finish structure for polynomial solver

--- scientific-calc/polynomial.m
+++ scientific-calc/polynomial.m
@@ -13,10 +13,7 @@ e = ones(size(target_matrix, 1), 1);
 b = polyval(p, target_matrix) * e;
 
 fprintf("Explicit: \n");
-x = [];
-tic
-x = polyvalm(p, target_matrix) \ b; % explicit
-toc
+tic; x = polyvalm(p, target_matrix) \ b; toc; % explicit
 
 error = norm(e - x, 2);
 fprintf("Explicit Error -> %f\n", error);
@@ -27,6 +24,7 @@ maxreps = 50;
 
 x = zeros(size(b, 1), size(p, 1));
 
+tic
 for i = 1:size(z,1)
    Alpha = (target_matrix - z(i)*I);
    beta = b;
@@ -35,7 +33,7 @@ for i = 1:size(z,1)
    end
    
    % solve Alpha x(i) = x(i-1)
-   %sol = Alpha \ beta;
+   % sol = Alpha \ beta;
    
    % pcg - no precondition
    %[sol ,flag,relres,iter] = pcg(Alpha, beta, tolerance, maxreps);
@@ -44,40 +42,37 @@ for i = 1:size(z,1)
    error = norm(e - x(:, 1), 2);
    fprintf("Iteration %d: Error -> %f\n", i, error);
 end
+toc
 
 
-% fprintf("Serial + %s\n", "backslash");
-% err_serial = 0;
-% for k = 1:1
-%     x = zeros(size(z, 2) - 1, size(target_matrix, 1));
-%     for i = 1:(size(z, 2) - 1)
-%         if i == 1
-%             right = b;
-%         else
-%             right = x(i-1, :)';
-%         end
-%         
-%         L_A = (target_matrix - p(i) * I);
-%         L_b = right;
-%         
-%         % backslash
-%         sol =  L_A \ L_b;
-%         
-%         % pcg - no precondition
-% %         [ans,flag,relres,iter] = pcg(L_A, L_b, tolerance, maxreps);
-% %         x(i, :) = ans;
-%         
-%         % pcg - jacobi precondition
-%         % normal jacobi: diag(diag(A))
-% %         M = blkdiag(A);
-% %         [ans, flag, relres, iter] = pcg(L_A, L_b, tolerance, maxreps, M);
-%         
-%         % cholesky matlab
-%         
-%         % cholesky mex
-%         
-%         x(i, :) = sol';
-%         err_serial = sum(abs(e - sol'));
-%         fprintf("error: %f\n", err_serial)
-%     end
-% end
+fprintf("pcg: \n");
+
+tic;[x ,~,~,~] = pcg(target_matrix, b, tolerance, maxreps);toc
+
+error = norm(e - x, 2);
+fprintf("pcg Error -> %f\n", error);
+
+return;
+fprintf("pcg (block jacobi): \n");
+
+M = [];
+tic;[x ,~,~,~] = pcg(target_matrix, b, tolerance, maxreps, M);toc
+
+error = norm(e - x, 2);
+fprintf("pcg (block jacobi) Error -> %f\n", error);
+
+return;
+fprintf("chol_btr: \n");
+
+tic;toc;
+
+error = norm(e - x, 2);
+fprintf("chol_btr Error -> %f\n", error);
+
+return;
+fprintf("chol_btr_mex: \n");
+
+tic;toc;
+
+error = norm(e - x, 2);
+fprintf("chol_btr_mex Error -> %f\n", error);
