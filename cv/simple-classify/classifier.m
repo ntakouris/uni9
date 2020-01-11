6f4b21f25458bb8c5f21fa467335e4eea414da39
Theodoros Ntakouris <zarkopafilis@gmail.com>
2020-01-11 11:03:35 +0200

    finish training and testing pipeline

--- cv/simple-classify/classifier.m
+++ cv/simple-classify/classifier.m
@@ -1,10 +1,8 @@
-convert2hsv = 0;
-
 fprintf("Reading images\n");
-train_im = load_from_dir('train', convert2hsv);
+train_im = load_from_dir('train');
 train_segm = load_from_dir('train_seg');
 
-test_im = load_from_dir('test', convert2hsv);
+test_im = load_from_dir('test');
 test_segm = load_from_dir('test_seg');
 
 
@@ -13,62 +11,59 @@ classnames = {'skin', 'noskin'}; % skin = 1, noskin = 0;
 Y = zeros([size(X, 1) 1]);
 
 fprintf("Preparing X and Y train\n");
-tic;
+tic
 for k = 1:numel(train_segm)
     seg = train_segm{k};
+    seg = seg / 255; % 1 or 0
     t = train_im{k};
     
-    seg = reshape(seg, [1080*1920 3]);
+    seg = reshape(seg, [1080*1920 1]);
     t = reshape(t, [1080*1920 3]);
     
-    idx = ((k-1) * (1080*1920)):(k*1080*1920);
+    idx = ((k-1) * (1080*1920))+1:(k*1080*1920);
     
-    X(idx) = t;
+    X(idx, :) = t;
     Y(idx) = seg;
 end
-toc;
+toc
 
 fprintf("Training\n");
-tic;
+tic
 cf = fitcnb(X, Y);
 % svm = fitcsvm(X, Y);
-toc;
+toc
 
 Y_Pred = zeros(size(Y));
 
 pred_segm = {};
 
 fprintf("Testing\n");
-tic;
+tic
 for k = 1:numel(test_segm)
     seg = test_segm{k};
     t = test_im{k};
     
-    seg = reshape(seg, [1080*1920 3]);
+    seg = reshape(seg, [1080*1920 1]);
     t = reshape(t, [1080*1920 3]);
     
-    idx = ((k-1) * (1080*1920)):(k*1080*1920);
+    idx = ((k-1) * (1080*1920) + 1):(k*1080*1920);
     
-    X(idx) = t;
+    X(idx, :) = t;
     Y(idx) = seg;
-    labels = predict(cf, X);
-    pred = zeros([length(idx) 1]);
+    labels = predict(cf, X(idx, :));
     
     % tresholding
-    trues = labels(:, 1) > 1.3 * labels(:, 2);
-    pred(trues) = 1;
-    
-    Y_Pred(idx) = pred;
-    pred_segm{k} = reshape(pred_seg, [1080 1920 1]);
-    visualize(reshape(t, [1080 1920 3]), pred_segm{k});
+    Y_Pred(idx) = labels;
+    pred_segm{k} = reshape(labels, [1080 1920 1]);
+    %visualize(reshape(t, [1080 1920 3]), pred_segm{k});
 end
-toc;
+toc
 
 fprintf("\n\nMetrics:\n\n");
-truepos = length(find(Y_Pred == 1 && Y == 1));
-falsepos = length(find(Y_Pred == 1 && Y == 0));
-falseneg = length(find(Y_Pred == 0 && Y == 1));
-trueneg = length(find(Y_Pred == 0 && Y == 0));
+truepos = length(find(Y_Pred == 1 & Y == 1));
+falsepos = length(find(Y_Pred == 1 & Y == 0));
+falseneg = length(find(Y_Pred == 0 & Y == 1));
+trueneg = length(find(Y_Pred == 0 & Y == 0));
 
 total = length(Y);
 
@@ -83,35 +78,23 @@ fprintf("Recall %f\n", recall);
 fprintf("F1 %f\n", f1);
 
 
-function imgs = load_from_dir(dir, varargin)    
-    convert = 0;
-    if nargin == 1
-        convert = varargin{1};
-    end
-    f = dir('train_seg');
-    imgs = cell([length(f) 1]);
+function imgs = load_from_dir(d)    
+    f = dir(d);
+    imgs = cell([length(f)-2 1]);
 
     for k = 3:length(f)
-      img = imread(f(k).name);
+      file = fullfile(f(k).folder, f(k).name);
+      img = imread(file);
       
-      if convert == 1
-        img = rgb2hsv(img);
-      end
+      % img = rgb2hsv(img);
       
       imgs{k-2} = img;
     end
 end
 
 function [] = visualize(img, seg)
-    vis = img;
-    if convert2hsv == 1    
-        vis = hsv2rgb(img);
-    end
-    
-    vis_r = vis .* seg .* [255 0 0];
-    
-    vis = vis * 0.5 + vis_r * 0.5;
-    
+    vis = im2double(img);
+    % vis = hsv2rgb(img);
     figure
-    imshow(vis)
+    imshow(vis .* seg)
 end
