8771041b3b129cfcd167e13ee1b88a3378a8dcc8
Theodoros Ntakouris <zarkopafilis@gmail.com>
2020-01-15 01:55:22 +0200

    cleanup and send it

--- scientific-calc/b2t.m
+++ scientific-calc/b2t.m
@@ -4,7 +4,7 @@ function T = b2t(A, m, n)
 
     for i = 1:m % n x n blocks
         for j = 1:m
-            %fprintf("A(%d:%d, %d:%d) -> T(:,:, %d, %d)\n", ((i-1) * m) + 1, i * m,(j-1) * m + 1, j * m , i, j);
+            fprintf("A(%d:%d, %d:%d) -> T(:,:, %d, %d)\n", ((i-1) * m) + 1, i * m,(j-1) * m + 1, j * m , i, j);
             T(:, :, i, j) = A(((i-1) * m) + 1:(i * m), ((j-1) * m) + 1:(j * m));
         end
     end
--- scientific-calc/katzBench.m
+++ scientific-calc/katzBench.m
@@ -1,53 +1,67 @@
 % Author: T. NTAKOURIS, AM 1054332, Date : 12/12/2019
-% load email
+% load email first
 tolerance = 1e-7;
 maxreps = 50;
 
-a = 0.01:0.01:1;
 I = eye(size(email));
 e = ones(size(email, 1), 1);
 
 % find min and max a that makes sense
-fprintf("Finding which a result to invertible matrix\n");
+% fprintf("Finding which a result to invertible matrix\n");
+% 
+valid_a = [];
+
 tic
-a = a(det(I - a * email') ~= 0 & (I - a * email') * e > 0);
+for a = 0.01:0.01:1
+    if det(I - a * email') ~= 0
+        if all((I - a * email') * e > 0)
+            valid_a = [valid_a a];
+        end
+    end
+end
 toc
 
-max_a = max(a);
-min_a = min(a);
+
+max_a = max(valid_a);
+min_a = min(valid_a);
 
 fprintf("max(a) = %f \nmin(a) = %f\n", max_a, min_a);
 
 a = [min_a max_a];
 
+fprintf("\n\n DIRECT ")
 method = "direct";
 params = {};
 X = multiKatz(email, a, method, params);
-fprintf("explicit (mina) ");
+fprintf("explicit (mina) \n");
 show_top5(X(1, :));
-fprintf("explicit (maxa) ");
+fprintf("explicit (maxa) \n");
 show_top5(X(end, :));
+
+fprintf("\n\n PCG ")
 
 method = "pcg";
 params = {tolerance, maxreps};
 X = multiKatz(email, a, method, params);
-fprintf("pcg no prec (mina) ");
+fprintf("pcg no prec (mina) \n");
 show_top5(X(1, :));
-fprintf("pcg no prec (maxa) ");
+fprintf("pcg no prec (maxa) \n");
 show_top5(X(end, :));
+
+fprintf("\n\n PCG ICHOL ")
 
 method = "pcg";
 params = {tolerance, maxreps, "ichol"};
 X = multiKatz(email, a, method, params);
-fprintf("pcg ichol prec (mina) ");
+fprintf("pcg ichol prec (mina) \n");
 show_top5(X(1, :));
-fprintf("pcg ichol prec (maxa) ");
+fprintf("pcg ichol prec (maxa) \n");
 show_top5(X(end, :));
 
 function [] = show_top5(x)
     m = maxk(x, 5);
     top5 = find(x >= m(end));
     top5 = sort(top5, 'descend');
-    
-    fprintf("top5: %d \n", top5);
+    fprintf("%d,", top5);
+    fprintf("\n");
 end
