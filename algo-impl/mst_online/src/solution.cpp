04b62e9ac8e2fc1d8a295220dc75ee8286942a68
Theodoros Zarkopafilis Ntakouris <zarkopafilis@gmail.com>
2020-05-27 22:59:33 +0300

    add framework for benchmarks

--- algo-impl/mst_online/src/solution.cpp
+++ algo-impl/mst_online/src/solution.cpp
@@ -1,4 +1,5 @@
 #define BENCH_TIMES 1
+#define DIFFERENT_EDGE_COST_TIMES 1
 
 #include <math.h>
 
@@ -15,6 +16,9 @@
 
 using namespace leda;
 static int z = 3;
+
+#define EDGE_COST_MIN -10000
+#define EDGE_COST_MAX 10000
 
 void BENCH_MST_LEDA(const graph &G, const edge_array<int> &cost) {
     timer dfs;
@@ -71,36 +75,8 @@ std::set<node> csearch(const graph &G, node &v, list<edge> &mst){
 
 }
 
-// void findclusters(const graph &G, list<edge> &t) {
-//     // find one leaf vertex of t
-    
-//     std::vector<std::set<node>> clusters;
-//     printed = clusters
-//     node root, n;
 
-//     // essentially a dfs variant
-//     std::set<node> cluster = csearch(n);
-//     if (clusters.size() > 0) {
-//         // merge with last printed 
-//         std::set<node> last_printed = printed.back();
-
-//         std::set<node>::iterator it;
-//         for (it = printed.begin(); it != printed.end(); ++it) {
-//             node n = *it;
-//             last_printed.insert(n);
-//         }
-//     }
-
-//     clusters = printed;
-//     // make map from clusters to idx
-
-//     // partition edges to E_i,j sets
-
-//     // map E_i,j sets to min cost map
-// }
-
-// paper implementation
-// builds the mst in a streaming fashion
+// builds the mst in a streaming-iterative fashion
 list<edge> MST_ONLINE(const graph &G, const edge_array<int> &cost){
     degree_fix(G, cost);
 
@@ -112,10 +88,10 @@ list<edge> MST_ONLINE(const graph &G, const edge_array<int> &cost){
     // build an initial mst
 
     // add rest of the vertices and edges
+    
 
     /* FINDCLUSTERS */
     node root; // = some leaf node of mst
-
 
     std::vector<std::set<node>> clusters;
     printed = clusters
@@ -149,32 +125,127 @@ list<edge> MST_ONLINE(const graph &G, const edge_array<int> &cost){
     /* case: not in mst and cost decreased */
 }
 
-void BENCH_MST_ONLINE(const graph &G, const edge_array<int> &cost){
-    timer dfs;
+
+void benchmark(const graph &G, const edge_array<int> &cost, int n, int m){
+    random_source S;
+
+    timer t;
     float elapsed_time = 0;
-    list<edge> ledas;
+    list<edge> mst;
 
     for (int i = 0; i < BENCH_TIMES; i++) {
-        // copy graph - we include degree_fix time
-        graph h;
-        CopyGraph(h, G);
-        edge_array<int> h_cost(h);
-        edge e;
-        forall_edges(e, h) {
-            // http://www.algorithmic-solutions.info/leda_manual/graph_misc.html
-            // ^ docs say that h[e] corresponds to G[e]
-            h_cost[e] = cost[h[e]];
-        }
+        t.start();
 
-        dfs.start();
+        mst = MIN_SPANNING_TREE(g, cost);
         
-        ledas = MST_ONLINE(h, h_cost);
-        
-        dfs.stop();
-        elapsed_time += dfs.elapsed_time();
+        t.stop();
+        elapsed_time += t.elapsed_time();
     }
 
-    std::cout << "OUR time: " << (elapsed_time / BENCH_TIMES) << std::endl;
+    std::cout << "MST construction time: " << (elapsed_time / BENCH_TIMES) << std::endl;
+
+    std::vector<edge> edges;
+    edge e;
+    forall_edges(e, G) {
+        edges.push_back(e);
+    }
+
+    // create random stream of edge costs to update
+    for (int i = 1; i < DIFFERENT_EDGE_COST_TIMES; i++) {
+        int s = m * i; // how many random edge-cost updates to do
+
+        std::vector<std::pair<edge, int>> cost_updates;
+
+        // generate random edge-cost pairs
+        int edge_size = edges.size();
+        for (int __x = 0; __x < s; __x++){
+            
+            int rand_idx;
+            S >> rand_idx;
+            rand_idx = rand_idx % edge_size;
+
+            int c;
+            S >> c;
+            int rand_cost = (c % (EDGE_COST_MAX – EDGE_COST_MIN + 1)) + EDGE_COST_MIN)
+
+            cost_updates.push_back(std::make_pair<edges[rand_idx], rand_cost>);
+        }
+
+        // bechmarks
+        float mst_online_time = 0;
+        float mst_static_time = 0;  
+
+        float mst_online_per_edge = 0;
+        float mst_static_per_edge = 0;
+
+        elapsed_time = 0;
+        float elapsed_per_edge = 0;
+
+        for (int i = 0; i < BENCH_TIMES; i++) {
+            // copy mst, G and edge costs so that there is a fresh copy
+            graph h;
+            CopyGraph(h, G);
+            edge_array<int> h_cost(h);
+            edge e;
+            forall_edges(e, h) {
+                // http://www.algorithmic-solutions.info/leda_manual/graph_misc.html
+                // ^ docs say that h[e] corresponds to G[e]
+                h_cost[e] = cost[h[e]];
+            }
+
+            // https://www.leda-tutorial.org/en/official/ch02s03s06.html
+            // ^ copy list
+            list<edge> h_mst = mst;
+
+            t.start();
+            
+            elapsed_per_edge += MST_ONLINE(h, h_cost, h_mst, cost_updates);
+            
+            t.stop();
+            elapsed_time += t.elapsed_time();
+        }
+
+        // std::cout << "MST_ONLINE time: " << (elapsed_time / BENCH_TIMES) << std::endl;
+        mst_online_time += elapsed_time;
+        mst_online_per_edge += elapsed_per_edge;
+
+        /* mst static */
+        elapsed_time = 0;
+        elapsed_per_edge = 0;
+
+        for (int i = 0; i < BENCH_TIMES; i++) {
+            // copy mst, G and edge costs so that there is a fresh copy
+            graph h;
+            CopyGraph(h, G);
+            edge_array<int> h_cost(h);
+            edge e;
+            forall_edges(e, h) {
+                // http://www.algorithmic-solutions.info/leda_manual/graph_misc.html
+                // ^ docs say that h[e] corresponds to G[e]
+                h_cost[e] = cost[h[e]];
+            }
+
+            // https://www.leda-tutorial.org/en/official/ch02s03s06.html
+            // ^ copy list
+            list<edge> h_mst = mst;
+
+            t.start();
+            
+            elapsed_per_edge += MST_STATIC(h, h_cost, h_mst, cost_updates);
+            
+            t.stop();
+            elapsed_time += t.elapsed_time();
+        }
+
+        // std::cout << "MST_STATIC time: " << (elapsed_time / BENCH_TIMES) << std::endl;
+        mst_static_time += elapsed_time;
+        mst_static_per_edge += elapsed_per_edge;
+    }
+
+    std::cout << std::endl << std::endl;
+
+    std::cout << "MST_ONLINE: " << (mst_online_time / DIFFERENT_EDGE_COST_TIMES) << " , per edge: " << (mst_online_per_edge / DIFFERENT_EDGE_COST_TIMES) << std::endl;
+    std::cout << "MST_STATIC: " << (mst_static_time / DIFFERENT_EDGE_COST_TIMES) << " , per edge: " << (mst_static_per_edge / DIFFERENT_EDGE_COST_TIMES) << std::endl;
 }
 
 int tests()
@@ -190,7 +261,7 @@ int tests()
 
         // generate connected graph with n nodes and m edges
         graph g;
-        rando (g, n, m);
+        random_loop_free_graph(g, n, m);
         Make_Connected(g);
         g.make_undirected();
 
@@ -199,13 +270,13 @@ int tests()
         forall_edges(e, g) {
             int c;
             S >> c;
-            cost[e] = 10 + c % 10000; // random cost range
+            cost[e] = (c % (EDGE_COST_MAX – EDGE_COST_MIN + 1)) + EDGE_COST_MIN);
         }
 
-        z = max(1, pow(m, 2/5));
+        z = max(1, pow(m, 2/5)); // see paper
 
         std::cout << "Random Graph (" << n << ", " << m << ") " << i << std::endl;
-        run_tests(g);
+        benchmark(g, cost, n, m);
     }
 
     return 0;
